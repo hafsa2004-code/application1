7159e361c4b1a25099dbda51f1e76fcd43be84e9
hafsaBlhc <hafsablhc@gmail.com>
2025-12-28 10:08:26 +0100

    Initial commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'firstApllication',
+      theme: ThemeData(),
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 84, 169, 211),
+        appBar: AppBar(
+          title: Center(child: const Text('Home')),
+          backgroundColor: Color.fromARGB(255, 69, 143, 180),
+        ),
+
+        body: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome Back !',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Color.fromARGB(255, 248, 248, 248),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Here is what is happening today:',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Color.fromARGB(255, 156, 150, 150),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Card(
+                  margin: EdgeInsets.zero,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  elevation: 5,
+                  color: Colors.white, // card background
+                  child: Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Your Tasks',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Row(
+                          children: [
+                            Icon(Icons.check_circle, color: Colors.green),
+                            SizedBox(width: 10),
+                            Text(
+                              'Meeting with team at 10 AM',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.check_circle_outline,
+                              color: Colors.orange,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              'Submit report by 3 PM',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Icon(Icons.cancel, color: Colors.red),
+                            SizedBox(width: 10),
+                            Text(
+                              'Call with client at 5 PM',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 10),
+              Padding(
+                padding: EdgeInsets.all(25.0),
+                child: Card(
+                  margin: EdgeInsets.zero,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  elevation: 5,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Progress',
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue,
+                              ),
+                            ),
+                            SizedBox(width: 110),
+                            CircularProgressIndicator(color: Colors.blue),
+                          ],
+                        ),
+
+                        Row(
+                          children: [
+                            Text(
+                              'Youre almost there',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 237, 238, 255), // Light purple background
+                  foregroundColor: Color.fromARGB(255, 94, 89, 198), // Text color
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 35,
+                    vertical: 15,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                  ),
+                ),
+                child: Text(
+                  'View More Details',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+            NavigationDestination(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
